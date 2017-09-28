5474c30b8d7aad2a3c54cd264c15535c050141e2
John.Darley <john.m.darley@gmail.com>
2017-09-28 10:04:02 +0100

    Pythin script changed to work in Lamdba

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,22 @@
+import boto3
+import StringIO
+import zipfile
+
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+
+    portfolio_bucket = s3.Bucket('portfolio.johndarley.info')
+    build_bucket = s3.Bucket('portfoliobuild.johndarley.info')
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip/portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+         for nm in myzip.namelist():
+             obj = myzip.open(nm)
+             portfolio_bucket.upload_fileobj(obj, nm)
+             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    # Comments section
+    print "Job Completed!!"
+    return 'Hello from Lambda'
