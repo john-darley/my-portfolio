592c1723708ff72534e8e6c6b91e295cf0788490
John.Darley <john.m.darley@gmail.com>
2017-09-29 11:32:53 +0100

    added a line to upload-portfolio that sets the content type for each file so that the web server knows how to serve it properly, rather than having it download

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,6 +1,7 @@
 import boto3
 import StringIO
 import zipfile
+import mimetypes
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
@@ -31,7 +32,7 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
              for nm in myzip.namelist():
                  obj = myzip.open(nm)
-                 portfolio_bucket.upload_fileobj(obj, nm)
+                 portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                  portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print "Job Completed!!"
